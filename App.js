cf6f0d01905a52d8caf6c7f223940f11e7addc1e
arsan13 <arsan.anbu97@gmail.com>
2022-01-27 21:44:58 +0530

    Add and update todo

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList } from "react-native-web";
+import { DATA } from "./DATA";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      tasks: DATA,
+    };
+  }
+
+  onAdd = () => {
+    const task = { id: Date.now(), text: this.state.text, isCompleted: false };
+    this.setState((state) => ({ tasks: [...state.tasks, task], text: "" }));
+  };
+
+  onCompletion = (id) => {
+    const updatedTasks = [];
+    for (let item of this.state.tasks) {
+      if (item.id !== id) updatedTasks.push(item);
+      else updatedTasks.push({ ...item, isCompleted: true });
+    }
+    this.setState({ tasks: updatedTasks });
+  };
+
+  renderItem = (data) => (
+    <View style={styles.item}>
+      <Text style={[styles.itemText, data.item.isCompleted && styles.overline]}>
+        {data.item.text}
+      </Text>
+      <Button
+        title="Done"
+        onPress={() => this.onCompletion(data.item.id)}
+        disabled={data.item.isCompleted ? true : false}
+      />
     </View>
   );
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.heading}>Simple Todo App</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter text for todo"
+          onChangeText={(val) => this.setState({ text: val })}
+          value={this.state.text}
+        />
+        <Button title="Add" onPress={this.onAdd} />
+        <FlatList
+          style={{ padding: 10 }}
+          data={this.state.tasks}
+          keyExtractor={(item) => item.id}
+          renderItem={(item) => this.renderItem(item)}
+        />
+      </View>
+    );
+  }
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  heading: {
+    marginTop: 10,
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 5,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  itemText: {
+    fontSize: 20,
+    padding: 5,
+  },
+  overline: {
+    textDecorationLine: "line-through",
   },
 });
---
+++ DATA.js
@@ -0,0 +1,12 @@
+export const DATA = [
+  {
+    id: 1,
+    text: "College Seminar",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    text: "Doctor Meeting",
+    isCompleted: true,
+  },
+];
